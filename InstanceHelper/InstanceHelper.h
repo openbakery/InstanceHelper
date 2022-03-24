f81147e67c507693ba15a250abf4c40efb1bd4d1
René Pirringer <rene@pirringer.at>
2022-03-24 11:11:50 +0100

    Initial Commit

---
+++ InstanceHelper/InstanceHelper.h
@@ -0,0 +1,18 @@
+//
+//  InstanceHelper.h
+//  InstanceHelper
+//
+//  Created by René Pirringer on 24.03.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstanceHelper.
+FOUNDATION_EXPORT double InstanceHelperVersionNumber;
+
+//! Project version string for InstanceHelper.
+FOUNDATION_EXPORT const unsigned char InstanceHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstanceHelper/PublicHeader.h>
+
+
